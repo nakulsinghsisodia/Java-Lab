cf3c2340f8b9dc6f630d5da81609eab918e8b08e
Nakul Singh Sisodia <153912261+nakulsinghsisodia@users.noreply.github.com>
2025-02-19 14:35:55 +0530

    Add files via upload

---
+++ EXP 3/ArSum.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.util.*;
+
+class ArSum{
+ public void sum(int a ,int b){
+ int sum = a+b;
+  System.out.printf("Sum is %d\n",sum);
+ }
+
+ public void sum(int a,int b,int c){
+ int sum = a+b+c;
+  System.out.printf("Sum is %d\n",sum);
+ }
+
+ public void sum(int a,int b,int c,int d){
+int sum = a+b+c+d;
+  System.out.print("Sum is "+sum);
+ }
+
+public static void main(String[] args){
+ ArSum a = new ArSum();
+ a.sum(5,5);
+a.sum(5,5,5);
+a.sum(5,5,5);
+
+}
+
+}
---
+++ EXP 3/Constructor.java
@@ -0,0 +1,40 @@
+import java.lang.*;
+import java.util.*;
+
+class Constructor{
+private int length;
+private int width;
+private int depth;
+
+Constructor(){
+this.length = 0;
+this.width = 0;
+this.depth = 0;
+}
+
+Constructor(int length,int width,int depth){
+this.length = length;
+this.width = width;
+this.depth = depth;
+}
+
+public int  volume(){
+int volume = 0;
+if (length == width && width == depth){
+volume = length*width*depth;
+}
+return volume;
+}
+
+public static void main(String[] args){
+Constructor obj = new Constructor(30,30,30);
+int volume = obj.volume();
+if(volume == 0){
+System.out.println("Not a Cubiod");
+}
+else{
+System.out.println("Volume is "+volume+" units");
+}
+}
+
+}
