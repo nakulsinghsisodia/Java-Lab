33997c1055f51f5c5decc1b8375834a1c5b78116
Nakul Singh Sisodia <153912261+nakulsinghsisodia@users.noreply.github.com>
2025-04-22 19:11:00 +0530

    Create Main2.java

---
+++ EXP 7/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught");
+        }
+    }
+}
+
+output: NullPointerException caught
