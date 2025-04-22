9cf1071cc918ee1920407e77fa0c542996987068
Nakul Singh Sisodia <153912261+nakulsinghsisodia@users.noreply.github.com>
2025-04-22 19:13:14 +0530

    Create Main6.java

---
+++ EXP 7/Main6.java
@@ -0,0 +1,14 @@
+public class Main6 {
+    public static void main(String[] args) {
+        try {
+            int a = 5 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception caught");
+        } finally {
+            System.out.println("Finally block always executes");
+        }
+    }
+}
+
+output: Arithmetic Exception caught
+Finally block always executes
